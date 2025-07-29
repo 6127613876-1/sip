9580132ab621af41830e1400bd9f3c426bd7c80d
Ridhyadharshinni <ridhyatr@gmail.com>
2025-07-29 22:18:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { LoginPage } from './pages/LoginPage';
 import { DashboardPage } from './pages/DashboardPage';
-import { AdminLoginPage } from './pages/AdminLoginPage';
-import { AdminDashboard } from './pages/AdminDashboard';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 
@@ -13,7 +11,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/admin-login" element={<AdminLoginPage />} />
         <Route 
           path="/admin" 
           element={
